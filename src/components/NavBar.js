a1fce18f6760c8fe39104c4f339aea0bb0e918df
AlexAntony3 <alex.antony.jy@gmail.com>
2023-11-22 21:05:06 +0000

    feat: create nav bar and customise using CSS

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import styles from '../styles/NavBar.module.css'
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import gbslogo from "../assets/gbslogo.png";
+
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={gbslogo} alt="logo" height="100" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
