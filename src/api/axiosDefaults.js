967c20d56f7926521b394497448d388ea6053b57
AlexAntony3 <alex.antony.jy@gmail.com>
2023-11-24 20:56:45 +0000

    feat: add usestate to the sign up form and customise

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
-axios.defaults.baseURL = 'https://glowbystef-40c11c2d1169.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://glowbystef-40c11c2d1169.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,47 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+  const [signUpInfo, setSignUpInfo] = useState({
+    username: "",
+    password: "",
+    confirm_password: "",
+  });
+  const { username, password, confirm_password } = signUpInfo;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setSignUpInfo({
+      ...signUpInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log("before axios")
+      await axios.post("/dj-rest-auth/registration/", signUpInfo);
+      console.log("after axios")
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
-            <Form.Group controlId="formBasicEmail">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
-                className={styles.Input}
+                className={`${styles.Input} ${styles.Placeholder}`}
                 type="text"
                 placeholder="Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Enter Password</Form.Label>
               <Form.Control
-                className={styles.Input}
+                className={`${styles.Input} ${styles.Placeholder}`}
                 type="password"
                 placeholder="Enter Password"
                 name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
 
-            <Form.Group controlId="confirm-password">
+            <Form.Group controlId="confirm_password">
               <Form.Label className="d-none">Enter Password</Form.Label>
               <Form.Control
-                className={styles.Input}
+                className={`${styles.Input} ${styles.Placeholder}`}
                 type="password"
                 placeholder="Confirm Password"
-                name="confirm-password"
+                name="confirm_password"
+                value={confirm_password}
+                onChange={handleChange}
               />
             </Form.Group>
 
